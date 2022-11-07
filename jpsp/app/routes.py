0fafcfdbd4dcb410665c8970f69d8c5904d6814d
Fabio Meneghetti <meneghetti.fabio@gmail.com>
2022-11-07 09:25:31 +0100

    sse

--- jpsp/app/routes.py
+++ jpsp/app/routes.py
@@ -5,6 +5,7 @@ from starlette.responses import RedirectResponse
 from starlette.routing import Route, Mount
 
 from jpsp.routes.api import routes as api_routes
+from jpsp.routes.sse import routes as sse_routes
 from jpsp.routes.socket import routes as socket_routes
 from jpsp.routes.static import routes as static_routes
 from jpsp.routes.webui import routes as webui_routes
@@ -21,6 +22,7 @@ routes = [
     Route('/', endpoint=redirect_endpoint),
     Mount('/web', routes=web_routes),
     Mount("/api", routes=api_routes),
+    Mount("/sse", routes=sse_routes),
     Mount("/socket", routes=socket_routes),
     Mount("/static", routes=static_routes),
     Mount("/webui", routes=webui_routes),
