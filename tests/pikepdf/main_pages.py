c576c6e9b6511bcebd7f7debef766d86860462ff
Fabio Meneghetti <meneghetti.fabio@gmail.com>
2022-12-22 09:52:06 +0100

    deps

---
+++ tests/pikepdf/main_pages.py
@@ -0,0 +1,55 @@
+"""
+
+Esecuzione parallela all'interno di un task group
+
+"""
+
+
+import time
+
+from anyio import run, to_process
+from anyio import open_file, run
+
+from jpsp.utils.http import download_stream
+
+from pdfminer.high_level import extract_text, extract_text_to_fp
+
+from io import StringIO
+from pdfminer.layout import LAParams
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+PDF = "/home/fabio.meneghetti/Projects/elettra/src/jpsp-ng/tests/pikepdf/AppleIII-Development_Tischer_FEL2022-THBI1.pdf"
+
+
+async def main():
+    
+    start = time.time()
+    
+    output = StringIO()
+    with open(PDF, 'rb') as fin:
+        extract_text_to_fp(fin, output, laparams=LAParams(),
+                            output_type='text', codec=None)
+        
+    print(output.getvalue())
+    
+    # create object
+    tfidf = TfidfVectorizer()
+
+    # get tf-df values
+    result = tfidf.fit_transform([output.getvalue()])
+    
+    # get idf values
+    print('\nidf values:')
+    for ele1, ele2 in zip(tfidf.get_feature_names_out(), tfidf.idf_):
+        print(ele1, ':', ele2)
+    
+    end = time.time()
+    
+    print(end - start)
+
+
