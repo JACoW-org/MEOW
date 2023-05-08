76197c23602f19f7b597222f731cae388883dd6e
fmeneghetti <meneghetti.fabio@gmail.com>
2023-05-08 22:55:41 +0200

    fix

--- meow/services/local/event/common/collecting_contributions_and_files.py
+++ meow/services/local/event/common/collecting_contributions_and_files.py
@@ -15,7 +15,7 @@ async def collecting_contributions_and_files(event: dict, sessions: list, cookie
 
     contributions: list = []
 
-    limiter = CapacityLimiter(8)
+    limiter = CapacityLimiter(16)
     async with create_task_group() as tg:
         for session in sessions:
             tg.start_soon(download_contributions, event.get('url'), session.get('id'), cookies, settings, contributions, limiter)
