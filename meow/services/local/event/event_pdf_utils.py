a2f35d14524964b0c1797c83dc002386b35859d9
fmeneghetti <meneghetti.fabio@gmail.com>
2023-05-11 09:13:50 +0200

    fix

--- meow/services/local/event/event_pdf_utils.py
+++ meow/services/local/event/event_pdf_utils.py
@@ -134,7 +134,7 @@ async def draw_frame(write_path: str, page: int, header: dict | None, footer: di
     cmd.append("-footer")
     cmd.append(json_encode(footer).decode('utf-8'))
     
-    logger.info('draw_frame', cmd)
+    logger.info(cmd)
 
     res = await run_cmd(cmd)
 
