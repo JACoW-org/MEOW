ffa64566a2b6ee3a892a41df5eff380702a2c5b7
fmeneghetti <meneghetti.fabio@gmail.com>
2023-05-30 15:32:29 +0200

    curl doi test

--- tests/dois/example.curl.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST -H \
-    "Content-Type: application/vnd.api+json" \
-    --user CERN.JACOW:DataCite.cub-gwd \
-    -d @example.json \
-    https://api.test.datacite.org/dois
---
+++ tests/dois/example.del.sh
@@ -0,0 +1,5 @@
+curl -X DELETE -H \
+    "Content-Type: application/vnd.api+json" \
+    --user CERN.JACOW:DataCite.cub-gwd \
+    -d @example.request.json \
+    https://api.test.datacite.org/dois/10.18429/jacow-ipac-23-moa03
