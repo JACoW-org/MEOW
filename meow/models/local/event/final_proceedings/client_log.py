3c5989601537dfe314fc04902e00d32843350e4b
Christian <christian.obetti@akera.it>
2023-06-13 12:27:57 +0200

    added enum for ClientLog severity attribute

--- meow/models/local/event/final_proceedings/client_log.py
+++ meow/models/local/event/final_proceedings/client_log.py
@@ -1,11 +1,19 @@
 from dataclasses import dataclass, field, asdict
 from datetime import datetime
+from enum import Enum
+
+class ClientLogSeverity(Enum):
+    INFO = 'info'
+    WARNING = 'warning'
+    ERROR = 'error'
 
 @dataclass
 class ClientLog:
-    severity: str
+    severity: ClientLogSeverity
     message: str
     timestamp: str = field(default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     def as_dict(self):
-        return asdict(self)
+        log_dict = asdict(self)
+        log_dict['severity'] = self.severity.value
+        return log_dict
