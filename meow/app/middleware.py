d3358e9f5b4e1e75de2b30fdf5025dd4962742a4
fmeneghetti <meneghetti.fabio@gmail.com>
2023-06-16 15:07:27 +0200

    fix

--- meow/app/middleware.py
+++ meow/app/middleware.py
@@ -2,12 +2,23 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 # from starlette.middleware.trustedhost import TrustedHostMiddleware
 
+origins = [
+    'http://127.0.0.1:8005',
+    'https://indico.jacow.org',
+    'https://indico.akera.net'
+]
+
+methods = ['*']
+headers = ['*']
+
+credentials = True
+
 middleware: list[Middleware] = [
     # Middleware(HTTPSRedirectMiddleware),
     # Middleware(TrustedHostMiddleware,
     #            allowed_hosts=['*.jacow.org']),
     # Middleware(CORSMiddleware, allow_origins=['*'])
-    Middleware(CORSMiddleware, allow_origins=['https://indico.jacow.org', 'https://indico.akera.net'], allow_methods=['*'],
-               allow_headers=['*'], allow_credentials=True)
+    Middleware(CORSMiddleware, allow_origins=origins,
+               allow_methods=methods, allow_headers=headers,
+               allow_credentials=credentials)
 ]
-
