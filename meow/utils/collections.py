25b99d2907a769cf91b4d66dfa7dbf4df7f6c53a
Christian <christian.obetti@akera.it>
2023-06-16 16:34:38 +0200

    Handled grouping strings that start with accents/variants or punctuation

--- meow/utils/collections.py
+++ meow/utils/collections.py
@@ -1,14 +1,20 @@
 import string
 
+from unidecode import unidecode
+
 from meow.models.local.event.final_proceedings.event_model import (AffiliationData, KeywordData,
     PersonData)
+from meow.callbacks.tasks import get
 
 def group_authors_by_last_initial_for_render(authors: list[PersonData]) -> dict:
     """"""
 
+    # set with letters from the alphabet
+    alphabet = set(string.ascii_lowercase)
+    
     groups = {}
     for author in authors:
-        key = author.last[0].lower() if author.last else author.first[0].lower()
+        key = get_initial(author.last, alphabet) if author.last else get_initial(author.first, alphabet)
         if key not in groups:
             groups[key] = []
         groups[key].append(author.as_dict())
@@ -16,10 +22,12 @@ def group_authors_by_last_initial_for_render(authors: list[PersonData]) -> dict:
 
 def group_keywords_by_initial(keywords: list[KeywordData]) -> dict:
     """"""
-
+    # set with letters from the alphabet
+    alphabet = set(string.ascii_lowercase)
+    
     groups = {}
     for keyword in keywords:
-        key = keyword.name[0].lower()
+        key = get_initial(keyword.name, alphabet)
         if key not in groups:
             groups[key] = []
         groups[key].append(keyword.as_dict())
@@ -28,11 +36,13 @@ def group_keywords_by_initial(keywords: list[KeywordData]) -> dict:
 
 def group_institutes_by_initial(institutes: list[AffiliationData]) -> dict:
     """"""
-
+    # set with letters from the alphabet
+    alphabet = set(string.ascii_lowercase)
+    
     groups = {}
     for institute in institutes:
         if institute.name:
-            key = institute.name[0].lower()
+            key = get_initial(institute.name, alphabet)
             if key not in groups:
                 groups[key] = []
             groups[key].append(institute.as_dict())
@@ -52,7 +62,7 @@ def get_authors_initials_dict(authors: list[PersonData]) -> dict[str, bool]:
 
     # iterate over authors to build map of initials
     for author in authors:
-        initial = author.last[0].lower() if author.last else author.first[0].lower()
+        initial = get_initial(author.last, alphabet) if author.last else get_initial(author.first, alphabet)
         initials[initial] = True
 
     return initials
@@ -70,7 +80,7 @@ def get_keywords_initials_dict(keywords: list[KeywordData]) -> dict[str, bool]:
         initials[letter] = False
 
     for keyword in keywords:
-        initial = keyword.name[0].lower()
+        initial = get_initial(keyword.name, alphabet)
         initials[initial] = True
 
     return initials
@@ -89,7 +99,18 @@ def get_institutes_initials_dict(institutes: list[AffiliationData]) -> dict[bool
 
     for institute in institutes:
         if institute.name:
-            initial = institute.name[0].lower()
+            initial = get_initial(institute.name, alphabet)
             initials[initial] = True
 
     return initials
+
+def get_initial(name, alphabet):
+    if not name:
+        return None
+    
+    initial = unidecode(name)[0].lower()
+
+    if initial not in alphabet:
+        return get_initial(name[1:])
+    
+    return initial
