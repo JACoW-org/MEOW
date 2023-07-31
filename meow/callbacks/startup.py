8fe11dfb0440786ba1607b1e2d93f860cac67ef0
fmeneghetti <meneghetti.fabio@gmail.com>
2023-07-31 13:58:40 +0200

    fix

--- meow/callbacks/startup.py
+++ meow/callbacks/startup.py
@@ -1,19 +1,25 @@
 import logging
 
+from anyio import Path
+
 logger = logging.getLogger(__name__)
 
 
 async def app_startup() -> None:
     logger.debug('Starlette startup - BEGIN')
 
-    from asyncio import create_task
-    from meow.callbacks.tasks import add
-
     async def _start_task() -> None:
         from meow.app.instances.application import app
         app.state.webapp_running = True
 
     await _start_task()
+
+    async def _mkdir_task() -> None:
+        await Path('var/cache').mkdir(parents=True, exist_ok=True)
+        await Path('var/html').mkdir(parents=True, exist_ok=True)
+        await Path('var/run').mkdir(parents=True, exist_ok=True)
+
+    await _mkdir_task()
 
     # async def _redis_task() -> None:
     #     from meow.app.instances.services import srs
@@ -29,6 +35,12 @@ async def app_startup() -> None:
         from meow.app.instances.services import srs
         await srs.socket_manager.run()
 
-    add(create_task(_socket_task()))
+    def _start_socket():
+        from asyncio import create_task
+        from meow.callbacks.tasks import add
+
+        add(create_task(_socket_task()))
+
+    _start_socket()
 
     logger.debug('Starlette startup - END')
